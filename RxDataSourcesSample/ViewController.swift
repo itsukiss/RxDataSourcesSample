681ababc84d591cc32bf25de343303b03af419ed
tanaka_itsuki <tanaka_itsuki@cyberagent.co.jp>
2018-07-23 11:23:41 +0900

    first commit

---
+++ RxDataSourcesSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxDataSourcesSample
+//
+//  Created by 田中 厳貴 on 2018/07/23.
+//  Copyright © 2018年 田中 厳貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
